f7cec8a661ec72ffe75cbc38724e4627ab69af1a
hemeemagunthoti <hemeemagunthoti@gmail.com>
2024-04-17 21:29:34 +0530

    FSD LAB

---
+++ array_objects/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TableView from './TableView';
+import './index.css';
+
+const App = () => {
+  const data = [
+    {ID : 1 , Name : 'John' , Age : 100000},
+    {ID : 2 , Name : 'Esther' , Age : 21},
+    {ID : 3 , Name : 'Emily' , Age : 20},
+    {ID : 4 , Name : 'Emory' , Age : 21},
+    {ID : 5 , Name : 'Lousie' , Age : 21},
+
+  ];
+
+  return (
+    <div>
+      <h1 style = {{ textAlign : 'center' , borderBottom : '2px solid blue' , paddingTop : '20px',marginBottom : '100px'}}>
+        Array of Objects - TableView
+      </h1>
+      <TableView data= {data} />
+    </div>
+  );
+};
+
+export default App;
